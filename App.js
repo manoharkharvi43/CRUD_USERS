73cd14614041dde15fcd5373b735653c6359b884
Manohar Kharvi <manohar@neokred.tech>
2021-06-30 17:36:39 -0700

    changes

--- App.js
+++ App.js
@@ -25,12 +25,10 @@ app.use(function (req, res, next) {
 });
 
 
-if(process.env.NODE_ENV==='production'){
-  app.use(express.static('client/build'))
-  app.get("*" , (req,res) =>{
-    res.sendFile(path.resolve(__dirname,'client' , "build" , "index.html"))
-  })
-}
+app.use(express.static(path.resolve(__dirname, "./client/build")));
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "./client/build", "index.html"));
+});
 app.use(express.json());
 app.use("/", route);
 
